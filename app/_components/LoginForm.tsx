192ddbc1952076484078b59b225144d171ff4f13
Gabriel Garcia <gabrieldev20x@gmail.com>
2023-08-25 00:34:52 -0500

    use Login Mutation, save the token in cache and redirect to chat-room
    Took 2 minutes

--- app/_components/LoginForm.tsx
+++ app/_components/LoginForm.tsx
@@ -5,8 +5,10 @@ import Text from "@/app/_desing-system/Text";
 import Button from "@/app/_desing-system/Button";
 import Input from "@/app/_desing-system/Input";
 import tw, {styled} from "twin.macro";
-import {ChangeEvent, FormEvent, useState} from "react";
+import {ChangeEvent, FormEvent, useEffect, useState} from "react";
 import {ILogin} from "@/types/Auth";
+import {useLoginMutation} from "@/api/auth-api";
+import {setAccessToken} from "@/lib/auth-token-cookies/authTokenCookies";
 
 const StyledForm = styled.form(() => [
     tw`flex flex-col gap-2`
@@ -16,10 +18,12 @@ function LoginForm() {
 
     const router = useRouter()
     const [loginData, setLoginData] = useState<ILogin>({username:'',password:''})
+    const [login, {data, isSuccess}] = useLoginMutation()
 
     const handleSubmit = (event: FormEvent) => {
         event.preventDefault()
         // TODO: Post Login data and recieve the access_token
+        login(loginData)
     }
 
     const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -31,6 +35,13 @@ function LoginForm() {
 
     // TODO: Detect if the login was success and save the access_token in the cache
     // TODO: on success, redirect to chat room
+    useEffect(() => {
+        if(data?.access_token) {
+            console.log(data)
+            setAccessToken(data)
+            router.push('/chat_room')
+        }
+    }, [isSuccess]);
 
     return (
         <StyledForm onSubmit={handleSubmit}>
