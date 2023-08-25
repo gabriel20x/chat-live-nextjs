443fa9c219a83a5274aa4de97eb88bcb73637dc1
Gabriel Garcia <gabrieldev20x@gmail.com>
2023-08-25 00:30:01 -0500

    Add apis to store
    Took 5 minutes

--- redux/store.ts
+++ redux/store.ts
@@ -1,17 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit'
 // Or from '@reduxjs/toolkit/query/react'
 import { setupListeners } from '@reduxjs/toolkit/query'
+import {messageApi} from "@/api/messages-api";
+import {authApi} from "@/api/auth-api";
+import {userApi} from "@/api/users-api";
 
 export const store = configureStore({
     reducer: {
         // Add the generated reducer as a specific top-level slice
         // TODO: add all the reducers example: [api.reducerPath]: api.reducer
+        [messageApi.reducerPath]: messageApi.reducer,
+        [authApi.reducerPath]: authApi.reducer,
+        [userApi.reducerPath]: userApi.reducer,
     },
     // Adding the api middleware enables caching, invalidation, polling,
     // and other useful features of `rtk-query`.
     middleware: (getDefaultMiddleware) =>
         // TODO: add the middleware to concat all the reducers. example:  getDefaultMiddleware().concat(api_1.middleware,api_2.middleware)
-        getDefaultMiddleware().concat(),
+        getDefaultMiddleware().concat(messageApi.middleware,authApi.middleware,userApi.middleware),
 })
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
